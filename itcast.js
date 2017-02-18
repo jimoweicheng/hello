96af9619e544a71e8d277518688680fb10446a0a
sun <1984707449@qq.com>
2017-02-18 18:35:14 +0800

    第一次完善jQuery‘ git commit -m 第一次完善jQuery‘

--- itcast.js
+++ itcast.js
@@ -13,6 +13,35 @@
         constructor: itcast,
         length: 0,
         splice: arr.splice,
+        toArray: function () {
+            return slice.call(this);
+        },
+        get: function (index) {
+            if (!index) {
+                return slice.call(this);
+            }
+            return this[index >= 0 ? index : index + this.length];
+        },
+        eq: function (index) {
+            if (!index) {
+                return slice.call(this);
+            }
+            return itcast(itcast.get(index));
+        },
+        first: function () {
+            return itcast(itcast.get(0));
+        },
+        last: function () {
+            return itcast(itcast.get(-1));
+        },
+        each: function (callback) {
+            return itcast.each(this, callback);
+        },
+        map: function (callback) {
+            return itcast(itcast.map(this, function (ele, i) {
+                return callback.call(ele, ele, i);
+            }));
+        }
 
     };
     init = itcast.fn.init = function (selector, context) {
@@ -23,15 +52,22 @@
             if (itcast.isHtml(selector)) {
                 push.call(this, itcast.parseHTML(selector));
             } else {
-                push.call(this,select(selector,context));
+                push.call(this, select(selector, context));
             }
-        }else if(isDom(selector)){
+        } else if (isDom(selector)) {
             this[0] = selector;
             this.length = 1;
-        }else if(itcast.isArrayLike(selector)){
-            push.call(this,selector);
-        }else if(){
-
+        } else if (itcast.isArrayLike(selector)) {
+            push.call(this, selector);
+        } else if (typeof selector === 'funciton') {
+            if (itcast.isReady) {
+                selector();
+            } else {
+                document.addEventListener('DOMConentLoaded', function () {
+                    itcast.isReady = true;
+                    selector();
+                });
+            }
         }
     }
     init.prototype = itcast.prototype;
@@ -42,33 +78,44 @@
     }
 
     itcast.extend({
+        isReady: false,
         isSting: function (obj) {
             return typeof obj === 'string';
         }, isHtml: function (obj) {
             return obj.charAt(0) === '<' && obj.charAt(obj.length - 1) === '>' && obj.length >= 3;
-        },isDom: function (obj) {
+        }, isDom: function (obj) {
             return
-        },isDom: function (obj) {
-            return 'nodetype' in obj && obj.nodeType ===1;
-        },isArrayLike: function (obj) {
-            var length =  !!obj && 'length' in obj && obj.length,type = typeof obj;
-            if(type==='function' && itcast.isWindow(obj)){
+        }, isDom: function (obj) {
+            return 'nodetype' in obj && obj.nodeType === 1;
+        }, isArrayLike: function (obj) {
+            var length = !!obj && 'length' in obj && obj.length, type = typeof obj;
+            if (type === 'function' && itcast.isWindow(obj)) {
                 return false;
             }
-            return typeof obj ==='array' ||length ===0 ||typeof length ==='number'&&length>0&&
-                    obj(length-1) in obj;
-        },isWindow: function (obj) {
-            return !! obj && obj.window === obj;
+            return typeof obj === 'array' || length === 0 || typeof length === 'number' && length > 0 &&
+                obj(length - 1) in obj;
+        }, isWindow: function (obj) {
+            return !!obj && obj.window === obj;
         }
     });
     itcast.extend({
         each: function (ele, callback) {
             for (var i = 0; i < ele.length; i++) {
-                if(callback(ele[i],ele[i],i)===false){
+                if (callback(ele[i], ele[i], i) === false) {
                     break;
                 }
             }
             return ele;
+        },
+        map: function (arr, callback, args) {
+            var value, ret = [];
+            for (var i = 0; i < arr.length; i++) {
+                value = callback(arr[i], i, args);
+                if (!value) {
+                    ret.push(value);
+                }
+            }
+            return Array.prototype.concat.apply([], ret);
         },
         parseHTML: function (obj) {
             var ret;
@@ -78,8 +125,77 @@
                 ret.push(div.childNodes[i]);
             }
             return ret;
+        },
+        type: function (obj) {
+            if (!obj) {
+                return obj + '';
+            }
+            return typeof obj === 'object' ? Object.prototype.toString.call(obj).slice(8, -1).toLowerCase :
+                typeof obj;
         }
     });
+
+    // DOM操作模块
+    itcast.fn.extend({
+        appendTo: function (target) {
+            var self = this, node, ret = [];
+            target.each(function (tele, i) {
+                self.each(function (sele) {
+                    node = i === 0 ? sele : sele.cloneNode(true);
+                    ret.push(node);
+                    tele.appendChild(sele);
+                })
+            });
+            return itcast(ret);
+        },
+        append: function (source) {
+            var text;
+            if (itcast.isSting(source) && !itcast.isHtml(source)) {
+                text = source;
+                source = itcast();
+                source[0] = document.createTextNode(text);
+                source.length = 1;
+            } else {
+                source = itcast(source);
+            }
+            source.appendTo(this);
+            return this;
+        },
+        prependTo: function (target) {
+            var firstChild, node, self = this, ret = [];
+            target = itcast(target);
+            target.each(function (tele,i) {
+                firstChild = tele.firstChild;
+                self.each(function (sele) {
+                    node = i===0?sele:sele.cloneNode;
+                    tele.insertBefore(node,firstChild)
+                    ret.push(node);
+                });
+            })
+            return itcast(ret);
+        },
+        prepend: function (source) {
+            var text;
+            if (itcast.isSting(source) && !itcast.isHtml(source)) {
+                text = source;
+                source = itcast();
+                source[0] = document.createTextNode(text);
+                source.length = 1;
+            } else {
+                source = itcast(source);
+            }
+            source.prependTo(this);
+            return this;
+        },
+        remove: function () {
+            return this.each(function (elem) {
+                this.parentNode.removeChild(this);
+            });
+        }
+
+    });
+
+
     var select = function (selector, context) {
         var ret = [];
         if (context) {
@@ -100,4 +216,9 @@
 
     }
     global.$ = global.itcast = itcast;
+    // 注册DOM树加载完毕的时间
+    // 用来更新itcast.isReady值
+    document.addEventListener('DOMContentLoaded', function() {
+        itcast.isReady = true;
+    });
 }(window));
